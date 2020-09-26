a578223226f48914aca4a0dd40a94e66f73babd7
luciana1faur <luciana_faur@yahoo.com>
2020-09-26 14:54:06 +0000

    Site Solution

--- main.cpp
+++ main.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
-
-int main() {
-  double a;
-
-  cout << "Compute the specified expressions and print the output: " << endl;
-  cout << "-------------------------------" << endl;
-  
-  a = (25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5);
-
-  cout << "Result of the expression (25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5) is " << a << endl;
-
-  return 0;
+ 
+int main()
+{
+    cout << "\n\n Compute the specified expressions and print the output:\n";
+	cout << "------------------------------------------------------------\n";
+    cout << " Result of the expression "<<"(25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5) is : "<< (25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5) <<"\n" ; 
 }
