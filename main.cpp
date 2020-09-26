dc0a834fb7dd619b6cf7140370078249367949c7
luciana1faur <luciana_faur@yahoo.com>
2020-09-26 14:52:35 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  double a;
+
+  cout << "Compute the specified expressions and print the output: " << endl;
+  cout << "-------------------------------" << endl;
+  
+  a = (25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5);
+
+  cout << "Result of the expression (25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5) is " << a << endl;
+
+  return 0;
+}
